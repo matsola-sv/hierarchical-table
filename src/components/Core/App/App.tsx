a0f39fd1cea4b92f6768e3a6a8494fc2495551ed
matsola-sv <sergey.macola@gmail.com>
2025-06-12 01:23:28 +0200

    feat(ui): add notistack for MUI toast notifications integration

--- src/components/Core/App/App.tsx
+++ src/components/Core/App/App.tsx
@@ -1,14 +1,18 @@
 import type { FC } from 'react';
 
+import { SnackbarProvider } from 'notistack';
+
 import AppRouter from '@/components/Core/Router/AppRouter';
 
 import './App.css';
 
 const App: FC = () => {
 	return (
-		<div className='App'>
-			<AppRouter />
-		</div>
+		<SnackbarProvider autoHideDuration={2000}>
+			<div className='App'>
+				<AppRouter />
+			</div>
+		</SnackbarProvider>
 	);
 };
 export default App;
