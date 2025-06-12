a4e9f65479e88525a82d8145f288485928eed949
matsola-sv <sergey.macola@gmail.com>
2025-06-12 02:46:43 +0200

    feat(auth): add OAuth login with Google and GitHub

---
+++ src/components/Auth/ProvidersOAuth/ProvidersOAuth.tsx
@@ -0,0 +1,56 @@
+import { useTranslation } from 'react-i18next';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import { useSnackbar } from 'notistack';
+
+import { OAuthProviders, authService, parseAuthError } from '@/services/auth/authService';
+
+import GoogleIcon from '@/components/Common/UI/Icons/GoogleIcon/GoogleIcon';
+
+import './ProvidersOAuth.css';
+
+const ProvidersOAuth = () => {
+	const { t } = useTranslation();
+	const { enqueueSnackbar } = useSnackbar();
+
+	const onAuthProvider = async (provider: OAuthProviders) => {
+		try {
+			const user = await authService.signInWithProvider(provider);
+			if (!user) {
+				throw new Error(`No user info received from provider: ${provider}`);
+				return;
+			}
+
+			enqueueSnackbar(
+				`Welcome, ${user.displayName}! Your email is: ${user.email}`,
+				{ variant: 'success' },
+			);
+		} catch (error) {
+			enqueueSnackbar(parseAuthError(error).message, {
+				variant: 'error',
+			});
+		}
+	};
+
+	return (
+		<div>
+			<h2>{t('components.auth.providerAuth.title')}:</h2>
+			<button
+				className='provider-btn google'
+				onClick={() => onAuthProvider(OAuthProviders.Google)}
+			>
+				<GoogleIcon />
+				<span>{t('components.auth.providerAuth.buttons.google')}</span>
+			</button>
+			<button
+				className='provider-btn github'
+				onClick={() => onAuthProvider(OAuthProviders.GitHub)}
+			>
+				<GitHubIcon />
+				<span>{t('components.auth.providerAuth.buttons.github')}</span>
+			</button>
+		</div>
+	);
+};
+
+export default ProvidersOAuth;
