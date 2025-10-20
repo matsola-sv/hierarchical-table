415d84d2d0c5e79dfb110f405164b2a6c7005f60
matsola-sv <sergey.macola@gmail.com>
2025-10-20 22:26:18 +0200

    fix: hide hierarchy on home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,16 +2,10 @@ import type { FC } from 'react';
 
 import { Box } from '@mui/material';
 
-import HierarchyView from '@/components/Hierarchy/HierarchyView/HierarchyView';
-
 /**
  * Avoid using plain <div> around MUI components — it may break layout or styling. Use <Box height='100%'> or <> instead
  */
 const HomePage: FC = () => {
-	return (
-		<Box height='100%'>
-			<HierarchyView />
-		</Box>
-	);
+	return <Box height='100%'>{/* <HierarchyView /> */}</Box>;
 };
 export default HomePage;
