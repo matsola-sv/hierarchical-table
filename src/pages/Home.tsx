5bf681595c03827d9fdfd0c2846f934af2a5d37c
matsola-sv <sergey.macola@gmail.com>
2025-10-20 17:30:09 +0200

    refactor: hide hierarchical table from Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -8,10 +8,6 @@ import HierarchyView from '@/components/Hierarchy/HierarchyView/HierarchyView';
  * Avoid using plain <div> around MUI components — it may break layout or styling. Use <Box height='100%'> or <> instead
  */
 const HomePage: FC = () => {
-	return (
-		<Box height='100%'>
-			<HierarchyView />
-		</Box>
-	);
+	return <Box height='100%'>{/* <HierarchyView /> */}</Box>;
 };
 export default HomePage;
